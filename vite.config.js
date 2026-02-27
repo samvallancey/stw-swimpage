50c5114a77208d36559fe61762cc8f768f9753e7
samvallancey <81306462+samvallancey@users.noreply.github.com>
2026-02-27 14:31:15 +0000

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,10 @@ import { defineConfig } from 'vite';
 
 export default defineConfig({
   base: '/stw-swimpage/',
+  build: {
+    outDir: 'docs',
+    emptyOutDir: true,
+  },
   server: {
     port: 8080,
     open: false,
